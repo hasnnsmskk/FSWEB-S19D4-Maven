6c2c0a6d4e0a88d9d3a97954a61c7323ecd664a6
Hasan Şimşek <168709291+hasnnsmskk@users.noreply.github.com>
2024-11-05 00:59:44 +0300

    S19D4 completed.

---
+++ src/main/java/com/workintech/s19d1/entity/Movie.java
@@ -0,0 +1,55 @@
+package com.workintech.s19d1.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name="movie", schema="fsweb")
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private long id;
+
+    @Column(name="name")
+    @NotBlank(message = "Movie name is mandatory")
+    private String name;
+
+    @Column(name="director_name")
+    @NotBlank(message = "Director name is mandatory")
+    private  String directorName;
+
+    @Column(name="rating")
+    @NotNull(message = "Rating is mandatory")
+    private Integer rating;
+
+    @Column(name="release_date")
+    private LocalDate releaseDate;
+
+
+    @ManyToMany(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH}, fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "movie_actor", schema="fsweb",
+            joinColumns = @JoinColumn(name = "movie_id"),
+            inverseJoinColumns = @JoinColumn(name = "actor_id")
+    )
+    private List<Actor> actors = new ArrayList<>();
+
+    public void addActor(Actor actor){
+        if(actors == null){
+            actors = new ArrayList<>();
+        }
+        actors.add(actor);
+    }
+
+}
